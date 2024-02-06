a00f64aa28f7e67a1d0da8a19b5866151872ddcf
Maria Angeles <maria.d.juri@gmail.com>
2024-02-06 15:18:28 -0500

    Style text and icons, add social media icons

--- src/IconLinks.jsx
+++ src/IconLinks.jsx
@@ -1,16 +1,17 @@
 import { LuDonut, LuDog } from "react-icons/lu";
 import { FaCode } from "react-icons/fa6";
 import { HiOutlinePaintBrush } from "react-icons/hi2";
-import { Group } from "@mantine/core";
+import { FiVideo } from "react-icons/fi";
 
 function IconLinks() {
   return (
     <>
-      <div className="row-of-icons-in-IconLinks">
-          <LuDonut size={150} />
-          <LuDog size={150} />
-          <HiOutlinePaintBrush size={150} />
-          <FaCode size={150}/>
+      <div className="row-of-icons">
+        <FaCode title="Code Symbol Icon" className="react-icons" />
+        <LuDonut title="Donut Icon" className="react-icons" />
+        <LuDog title="Dog Icon" className="react-icons" />
+        <HiOutlinePaintBrush title="Paint Brush Icon" className="react-icons" />
+        <FiVideo title="Video Icon" className="react-icons" />
       </div>
     </>
   );
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,15 +1,20 @@
 import IconLinks from "./IconLinks"
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
 
 function LandingPage () {
 console.log('landing page')
     
     return (
         <>
-        <div className="landing-page-text">
-        <h1>Don&#39;t worry, I&#39;m a professional...</h1>
-        </div>
+        <h1 className="Title">Don&#39;t worry, I&#39;m a professional...</h1>
+        <div><p>My name is Maria Angeles and I spend most of my time in the uncomfortable and magnificent spaces where ideas become reality. Check out some of my projects!</p></div>
         <div><IconLinks /></div>
-        <div><p>I spend most of my time in the uncomfortable and magnificent spaces where ideas become reality.</p></div>
+        <div className="social-media-icons">
+            <FaInstagram title="Instagram Icon" />
+            <FaLinkedin title="LinkedIn Icon" />
+            <IoLogoGithub title="GitHub Icon" />
+        </div>
         </>
     )
 }
