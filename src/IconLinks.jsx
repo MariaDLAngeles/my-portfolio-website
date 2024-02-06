7bcc1194345324067352feb12619604497ab1b44
Maria Angeles <maria.d.juri@gmail.com>
2024-02-06 16:01:40 -0500

    style icons and add text, next is links to pages

--- src/IconLinks.jsx
+++ src/IconLinks.jsx
@@ -2,16 +2,25 @@ import { LuDonut, LuDog } from "react-icons/lu";
 import { FaCode } from "react-icons/fa6";
 import { HiOutlinePaintBrush } from "react-icons/hi2";
 import { FiVideo } from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+import { FaSquareInstagram } from "react-icons/fa6";
+
 
 function IconLinks() {
   return (
     <>
-      <div className="row-of-icons">
-        <FaCode title="Code Symbol Icon" className="react-icons" />
-        <LuDonut title="Donut Icon" className="react-icons" />
-        <LuDog title="Dog Icon" className="react-icons" />
-        <HiOutlinePaintBrush title="Paint Brush Icon" className="react-icons" />
-        <FiVideo title="Video Icon" className="react-icons" />
+      <div className="project-icons-row">
+        <FaCode title="Code Symbol Icon" className="project-icons" />
+        <LuDonut title="Donut Icon" className="project-icons" />
+        <LuDog title="Dog Icon" className="project-icons" />
+        <HiOutlinePaintBrush title="Paint Brush Icon" className="project-icons" />
+        <FiVideo title="Video Icon" className="project-icons" />
+        </div>
+        <div className="social-media-icons-row">
+        <a href="https://www.instagram.com/doughnut_days/"><FaSquareInstagram title="Instagram Icon" className="social-media-icons"/></a>
+        <FaLinkedin title="LinkedIn Icon" className="social-media-icons"/>
+        <IoLogoGithub title="GitHub Icon" className="social-media-icons"/>
       </div>
     </>
   );
--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,6 +1,5 @@
 import IconLinks from "./IconLinks"
-import { FaInstagram, FaLinkedin } from "react-icons/fa";
-import { IoLogoGithub } from "react-icons/io";
+
 
 function LandingPage () {
 console.log('landing page')
@@ -8,13 +7,10 @@ console.log('landing page')
     return (
         <>
         <h1 className="Title">Don&#39;t worry, I&#39;m a professional...</h1>
-        <div><p>My name is Maria Angeles and I spend most of my time in the uncomfortable and magnificent spaces where ideas become reality. Check out some of my projects!</p></div>
+        <div><p>My name is Maria Angeles and I spend most of my time in the uncomfortable and magnificent spaces where ideas become reality.</p></div>
+        <p>Check out some of my projects and connect with me on Social Media!</p>
         <div><IconLinks /></div>
-        <div className="social-media-icons">
-            <FaInstagram title="Instagram Icon" />
-            <FaLinkedin title="LinkedIn Icon" />
-            <IoLogoGithub title="GitHub Icon" />
-        </div>
+        <p className="bottom-of-landing-page-text">Made with love and React because I&#39;m just scared enough of everything else.</p>
         </>
     )
 }
